4b2a5d8e2ab7900df9546f72a94e72d6c30caaed
Gabriel Verdan <149999884+GVerdans@users.noreply.github.com>
2024-08-14 21:10:56 -0300

    Add files via upload
    add Operadores Logicos file.

--- N1/Operadores Aritmeticos/OperadoresAritmeticos.php
+++ N1/Operadores Aritmeticos/OperadoresAritmeticos.php
@@ -19,26 +19,25 @@
 
     if ($operacao == "soma") {
         $soma = $valor1 + $valor2;
-        echo "<h3>A Soma de $valor1 e $valor2 é igula a $soma. </h3>";
+        echo "<h3>A Soma de $valor1 e $valor2 é igual a $soma. </h3>";
 
     } else if ($operacao == "subtracao") {
         $subtracao = $valor1 - $valor2;
-        echo "<h3>A Subtração de $valor1 e $valor2 é igula a $subtracao. </h3>";
+        echo "<h3>A Subtração de $valor1 e $valor2 é igual a $subtracao. </h3>";
 
     } else if ($operacao == "multiplicacao") {
         $multiplicacao = $valor1 * $valor2;
-        echo "<h3>A Multiplicação de $valor1 e $valor2 é igula a $multiplicacao. </h3>";
+        echo "<h3>A Multiplicação de $valor1 e $valor2 é igual a $multiplicacao. </h3>";
 
     } else {
         $divisao = $valor1 / $valor2;
-        echo "<h3>A Divisão de $valor1 e $valor2 é igula a $divisao. </h3>";
+        echo "<h3>A Divisão de $valor1 e $valor2 é igual a $divisao. </h3>";
     }
 
 
         // $resto = $valor1 % $valor2;
         // echo "<h3>O Resto de $valor1 e $valor2 é igula a $resto. </h3>";
     ?>
-
 </body>
 
 </html>
