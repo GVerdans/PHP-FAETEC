d14e72273d087b0aa7948c0d12e5a81489b93097
GVerdans <149999884+GVerdans@users.noreply.github.com>
2024-08-15 16:39:57 -0300

    ajustadas as pastas e deixei o html mais bonito

--- N1/MaiorValor/maiorValor.php
+++ N1/MaiorValor/maiorValor.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Operadores Lógicos</title>
+    <title>Resultado Maior Valor</title>
 
-    <link rel="stylesheet" href ="./styles/styles.css">
+    <link rel="stylesheet" href="./styles/style.css">
 </head>
 
 <body>
@@ -16,9 +15,9 @@
     // || - OU
     // ! - NOT
     
-    $num1 = 1000;
-    $num2 = 100;
-    $num3 = 500;
+    $num1 = $_POST["ipt_number1"];
+    $num2 = $_POST["ipt_number2"];
+    $num3 = $_POST["ipt_number3"];
 
     if ($num1 == $num2 || $num1 == $num3 || $num2 == $num3) {
         echo "<h3> Os valores precisam ser diferentes ! </h3>";
