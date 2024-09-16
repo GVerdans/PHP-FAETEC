f581c2347ace0df16e9a99d69d9974a1ff12582c
Gabriel Verdan <149999884+GVerdans@users.noreply.github.com>
2024-09-16 20:19:34 -0300

    Add files via upload
    Aula dia 16SET

--- N1/Exercicios/IMC/controller_IMC.js
+++ N1/Exercicios/IMC/controller_IMC.js
@@ -1,14 +1,32 @@
 $(function() {
-    $("#btn").click(function() {
-        let peso = $("#").val();
-        let altura = $("#").val();
+    $("#btn_calcular").click(function() {
+        let nome = $("#nome").val();
+        let peso = $("#peso").val();
+        let altura = $("#altura").val();
 
         // Podem entrar validações aqui
+        // console.log(nome, peso, altura)
+
+        if(nome == ""){
+            alert('Insira seu nome !');
+            return false;
+        } else if (peso == ""){
+            alert('Insira seu Peso !');
+            return false;
+        } else if (altura == ""){
+            alert('Insira sua Altura !')
+            return false;
+        }
+
+        if (peso < 0 || altura < 0){
+            alert('Insira valores acima de 0 !')
+            return false;
+        }
 
         $.ajax({
             type: "POST",
             url: "controller_IMC.php",
-            data: `peso = ${peso} &altura = ${altura}`,
+            data: `nome=${nome}&peso=${peso} &altura=${altura}`,
             success: function(msg) {
                 $("#resultado").html(msg);
             },
--- N1/Exercicios/IMC/controller_IMC.php
+++ N1/Exercicios/IMC/controller_IMC.php
@@ -3,19 +3,17 @@
 include 'IMC.php';
 
 $IMC = new IMC();
-$peso = 90; // $_POST depois
-$altura = 1.75; // $_POST depois
+
+$nome= ucfirst($_POST['nome']);
+$peso = $_POST['peso']; 
+$altura = $_POST['altura'];
 
 
 $valorDoIMC = $IMC->calcularIMC($peso, $altura); // Caso dê xabu, mudar o nome da variavel.
 $VRFSituacao = $IMC->verificarSituacao($valorDoIMC);
 $descricaoDoIndice = $IMC->descricao($VRFSituacao);
 
-$mensagem = "
-O seu IMC foi: $valorDoIMC <br><br>
-A sua situação é: $VRFSituacao <br>
-$descricaoDoIndice
-";
+$mensagem = "<p>Olá $nome, seu IMC deu <b>$valorDoIMC</b>, logo sua situação é de <b>$VRFSituacao</b> o que quer dizer:</p><br> $descricaoDoIndice.";
 
 echo $mensagem;
 exit
