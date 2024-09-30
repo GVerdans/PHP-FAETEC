a34b5157ad18c9db380dcb58dabd8276c75fc673
Gabriel Verdan <149999884+GVerdans@users.noreply.github.com>
2024-09-30 20:36:51 -0300

    Criada Pasta Repetição
    Loop FOR.

---
+++ N1/function/Repeticao/loopFor.php
@@ -0,0 +1,38 @@
+<?php
+/*
+for($i = 0 ; $i <= 50 ; $i += 2 ){
+    echo "$i <br>";
+}
+*/
+
+/*
+for($i = 0 ; $i <= 50 ; $i++){
+    if($i % 2 == 0){
+        echo "$i eh <b><font color=red>par</font></b><br>";
+    } else {
+        echo "$i eh <font color=blue><b>impar</b></font><br>";
+    }
+}
+*/
+
+/*
+for($i =20 ; $i >=1 ; $i--){
+    echo "$i<br>";
+}
+*/
+
+/*
+for($i = 1 ; $i<=100 ; $i++){
+
+    if($i < 10){
+        echo "0$i &nbsp";
+    }else{
+        echo "$i &nbsp";
+    }
+    
+    if($i % 10 == 0){
+        echo "<br>";
+    }   
+}
+*/
+?>
